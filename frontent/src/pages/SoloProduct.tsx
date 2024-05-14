4a87c26db9b5ce4322e788bbd8643f36457d78a0
Carlos Gamayo <carlos.gamayo.leon@gmail.com>
2024-05-14 19:18:11 +0200

    la ventana de visualizar producto está correcta

--- frontent/src/pages/SoloProduct.tsx
+++ frontent/src/pages/SoloProduct.tsx
@@ -3,11 +3,11 @@ import Loader from "../components/Loader";
 import { get_product } from "../api/products";
 import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
-
+import { StarIcon } from "@heroicons/react/16/solid";
 
 const SoloProduct = () => {
 
-    const  { slug } = useParams();
+    const { slug } = useParams();
 
     let new_slug: string;
     if (slug !== undefined) {
@@ -19,7 +19,7 @@ const SoloProduct = () => {
         queryKey: ["product", slug],
         queryFn: () => get_product(new_slug),
     })
-    
+
     console.log(data);
 
     if (isLoading) {
@@ -30,30 +30,82 @@ const SoloProduct = () => {
         return toast.error("Error!");
     }
 
-    return (
-        <div className="bg-white dark:bg-gray-900">
-            <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-                <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-                        {data.name}
-                    </h2>
-                    <p className="mb-4 font-bold">
-                        {data.description}
-                    </p>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Add to Cart
-                        <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd">
-                            </path>
-                        </svg>
-                    </a>
-                </div>
+    function classNames(...classes: (false | null | undefined | string)[]) {
+        return classes.filter(Boolean).join(' ')
+    }
 
-                <img className="w-full"
-                    src={`${import.meta.env.VITE_BAKEND_URL}${data.image}`}/>
+    return (
+
+        <div className="bg-white dark:bg-gray-900">
+            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
+                    {/* Image */}
+                    <div className="flex flex-col-reverse">
+                        <div className="w-full aspect-w-1 aspect-h-1">
+                            <img
+                                src={`${import.meta.env.VITE_BAKEND_URL}${data.image}`}
+                                className="w-full h-full object-center object-cover sm:rounded-lg"
+                            />
+                        </div>
+                    </div>
+                {/* Product info */}
+                <div className="mt-10 px-4 sm:px-0 sm:mt-16 lg:mt-0">
+                    <h1 className="text-3xl font-extrabold tracking-tight text-blue-300">{data.name}</h1>
+
+                    <div className="mt-3">
+                        <h2 className="sr-only text-white dark:text-white">{data.description}</h2>
+                        <p className="text-3xl text-green-300">€{data.price}</p>
+                    </div>
+
+                    {/* Reviews */}
+                    <div className="mt-3">
+                        <h3 className="sr-only">Reviews</h3>
+                        <div className="flex items-center">
+                            <div className="flex items-center">
+                                {[0, 1, 2, 3, 4].map((rating) => (
+                                    <StarIcon
+                                        key={rating}
+                                        className={classNames(
+                                            data.rating > rating ? 'text-indigo-500' : 'text-gray-300',
+                                            'h-5 w-5 flex-shrink-0'
+                                        )}
+                                        aria-hidden="true"
+                                    />
+                                ))}
+                            </div>
+                            <p className="sr-only">{data.rating} out of 5 stars</p>
+                        </div>
+                    </div>
+
+                    <div className="mt-6">
+                        <h3 className="sr-only">Description</h3>
+
+                        <div
+                            className="text-base text-gray-700 space-y-6"
+                            dangerouslySetInnerHTML={{ __html: data.description }}
+                        />
+                    </div>
+
+                    <form className="mt-6">
+                        <div className="mt-10 flex sm:flex-col1">
+                            <button
+                                type="submit"
+                                className="max-w-xs flex-1 bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 sm:w-full"
+                            >
+                                Añadir al carrito
+                            </button>
+                        </div>
+                    </form>
+
+                    <section aria-labelledby="details-heading" className="mt-12">
+                        <h2 id="details-heading" className="sr-only">
+                            Additional details
+                        </h2>
+                    </section>
+                </div>
             </div>
         </div>
+        </div >
     );
-
 }
 export default SoloProduct;
